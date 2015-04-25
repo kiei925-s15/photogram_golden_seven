9d45d66ae54489edefa774d2bf1a1e456208ccd1
snazaran <snazaran@gmail.com>
2015-04-25 11:01:52 -0500

    finish Read section

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,17 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    ident = params[:id]
+
+    @photo = Photo.find(ident)
+
+  end
+
+  def new_form
+  end
+
+
+
 
 end
