d027996a82ac5e6aada773ec7434db718538295c
Jacob Katz <jacob-katz@kellogg.northwestern.edu>
2015-04-26 17:21:36 -0500

    edit.complete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,17 +2,18 @@ class PhotosController < ApplicationController
   def index
     @list_of_photos = Photo.all
   end
-  
+
   def show
   	@show_photo = Photo.find(params["id"])
   end
 
   def new
 
+
   end
 
   def create_row
-  	z = Photo.new 
+  	z = Photo.new
 
   	z.caption = params[:the_caption]
   	z.source = params[:the_source]
@@ -27,6 +28,26 @@ class PhotosController < ApplicationController
   	Photo.find(params["id"]).destroy
 
 		redirect_to("http://localhost:3000/photos")
-		  	
   end
+  def edit_form
+    z = Photo.find(params["id"])
+
+    @caption = z.caption
+    @source = z.source
+    @id = z.id
+  end
+
+  def update_row
+    z = Photo.find(params[:id])
+
+    z.caption = params["the_caption"]
+    z.source = params["the_source"]
+
+    z.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
 
+  get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
+
 	get("/create_photo", { :controller => "photos", :action => "create_row" })
 
   get("/photos/:id",       { :controller => "photos", :action => "show" })
