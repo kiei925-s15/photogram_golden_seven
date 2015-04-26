4b550631f192b1d5f868ddeb28acb16a7563f5b6
Jacob Katz <jacob-katz@kellogg.northwestern.edu>
2015-04-25 21:47:42 -0500

    part1.complete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,16 @@ class PhotosController < ApplicationController
   def index
     @list_of_photos = Photo.all
   end
+  
+  def show
+  	@show_photo = Photo.find(params["id"])
+  end
 
+  def new_form
 
+  end
+
+  def create
+
+  end
 end
