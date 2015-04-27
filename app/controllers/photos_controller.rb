f7b7a41ddd7cb7c60e08804883617f09fa276820
sinchanlad <sinchanlad@hotmail.com>
2015-04-26 23:51:41 -0500

    update pics

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -19,6 +19,19 @@ class PhotosController < ApplicationController
     redirect_to("http://localhost:3000/photos")
   end
 
+  def edit_form
+    @edit_row =Photo.find(params[:id])
+    @row_id = params[:id]
+  end
+
+  def update_photo
+    update_row = Photo.find(params[:id])
+    update_row.caption = params[:the_caption]
+    update_row.source = params[:the_source]
+    update_row.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
   def destroy
     destroy_row = Photo.find(params[:id])
     destroy_row.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get("/photos/new", { :controller => "photos", :action => "new_form" })
   get("/create_photo", { :controller => "photos", :action => "create_row" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+  get("/update_photo/:id", { :controller => "photos", :action => "update_photo" })
   get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
 
 end
