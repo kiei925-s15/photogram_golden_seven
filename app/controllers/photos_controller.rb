a811b8ed1b43751741635b544ba80535873aa6f7
sinchanlad <sinchanlad@hotmail.com>
2015-04-26 20:07:11 -0500

    addpics

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,10 +1,22 @@
 class PhotosController < ApplicationController
+
   def index
     @list_of_photos = Photo.all
   end
+
   def show
     @show_photo = Photo.find_by({:id => params[:id]})
+  end
 
+  def new_form
+  end
+
+  def create_row
+    new_row = Photo.new
+    new_row.caption = params[:the_caption]
+    new_row.source = params[:the_source]
+    new_row.save
+    redirect_to("http://localhost:3000/photos")
   end
 
 end
