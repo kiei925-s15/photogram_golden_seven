c4ce92ff8128cf1da16e93ec17de6c08d4d9b365
Eric Bellendir <e-bellendir@kellogg.northwestern.edu>
2015-04-26 22:20:43 -0500

    view add and delete complete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,29 @@
 class PhotosController < ApplicationController
   def index
     @list_of_photos = Photo.all
-  end
+end
 
+def new_form
+end
 
+def create_row
+    p = Photo.new
+    p.source = params[:the_source].downcase
+    p.caption = params[:the_caption].downcase.capitalize
+    p.save
+    redirect_to("http://localhost:3000")
+end
+
+def show
+    @p = Photo.find(params[:id])
+    @p_source = @p["source"]
+    @p_caption = @p["caption"]
+end
+
+def delete_row
+    p = Photo.find(params[:id])
+    p.destroy
+    p.save
+    redirect_to("http://localhost:3000")
+end
 end
