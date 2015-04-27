72a496da7cc55a046b2abda4f21f7b87493b01af
Brad Barron <b-barron@kellogg.northwestern.edu>
2015-04-27 18:22:26 -0500

    draft 4-27

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,43 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo_show = Photo.find_by({:id => params["id"]})
+    @img = @photo_show.source
+    @cap = @photo_show.caption
+    render 'show'
+  end
 
+  def new_form
+    render 'new_form'
+  end
+
+  def create_row
+    z = Photo.new
+    z.caption = params[:photo_caption]
+    z.source = params[:photo_image_url]
+    z.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+     @photo_destroy = Photo.find_by({:id => params["id"]})
+     @photo_destroy.destroy
+     redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @edit = Photo.find_by({:id => params["id"]})
+    @img = @edit.source
+    @cap = @edit.caption
+    render 'edit_form'
+  end
+
+  def update_row
+    z = Photo.find_by({:id => params["id"]})
+    z.caption = params[:the_new_caption]
+    z.source = params[:the_new_source]
+    z.save
+    redirect_to("http://localhost:3000/photos")
+  end
 end
