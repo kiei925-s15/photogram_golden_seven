1a955e7d31ccc896b490c72b4dd024a96dae1860
M. Abraham <m-abraham@kellogg.northwestern.edu>
2015-04-27 23:40:37 -0500

    Edit Photos Saved

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -22,8 +22,17 @@ class PhotosController < ApplicationController
 
   def destroy
     photo = Photo.find(params[:id])
-    photo.destroy=params[:delete_row]
-redirect_to("http://localhost:3000/photos")
+    photo.destroy=params["delete_row"]
+
+    redirect_to("http://localhost:3000/photos")
 end
 
+  def edit_form
+    @photo = Photo.find(params["id"])
+    @photo.source=@photo["source"]
+    @photo.caption=@photo["caption"]
+    @photo.save
+  end
+
 end
+
